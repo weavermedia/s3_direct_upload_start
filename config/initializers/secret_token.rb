1430748ef4406a5cc45a5a3ca60a9fa9142df9ce
Daniel Weaver <danweavermedia@gmail.com>
2013-08-10 17:42:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S3DirectUploadStart::Application.config.secret_token = '9e6609118bd1e758670c65d00b5232fa0d3b2f286469367dcf8819be6f0204c4c0ae26efdd7b4fea9b8dd41b5b8e5b1fbd888a26b24362b5f7c4940933701595'
